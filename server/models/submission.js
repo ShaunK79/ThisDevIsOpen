3030264155ee2270a67abf039ee0b62705b8d5ba
ShaunK79 <wism79@gmail.com>
2025-04-17 15:08:22 +0100

    moved files from public to root for GitHub pages

--- server/models/submission.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const submissionSchema = new mongoose.Schema({
-    name: String,
-    message: String,
-    date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('submission', submissionSchema);
